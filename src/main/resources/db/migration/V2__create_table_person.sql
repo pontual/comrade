78de8b0a88e64cb2c7d0056f5d60217104af7638
Thiago Alves <thiago.balves@hotmail.com>
2025-06-05 07:50:38 -0300

    Criaçao de migrations relacionadas ao cadastro de pessoas e dados de acesso

---
+++ src/main/resources/db/migration/V2__create_table_person.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS sch_person.person (
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    document    VARCHAR(20) NOT NULL UNIQUE,
+    email       VARCHAR(255),
+    phone       VARCHAR(20),
+    created_at  TIMESTAMP NOT NULL,
+    updated_at  TIMESTAMP NOT NULL
+);
+
+ALTER TABLE  sch_person.person
+    OWNER TO "UserPontual";
+
+CREATE INDEX IF NOT EXISTS idx_document_person
+    ON sch_person.person (document);
---
+++ src/main/resources/db/migration/V6__create_table_account_user.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS sch_user.account_user (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role     VARCHAR(30) NOT NULL,
+    person_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (person_id) REFERENCES sch_person.person(id)
+);
+
+ALTER TABLE  sch_user.account_user
+    OWNER TO "UserPontual";
+
+CREATE INDEX IF NOT EXISTS idx_username_account_user
+    ON sch_user.account_user (username);
