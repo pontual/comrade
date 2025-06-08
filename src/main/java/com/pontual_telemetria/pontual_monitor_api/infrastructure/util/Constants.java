65440351af36730faf1942e1afeff7b37d82d022
Thiago Alves <thiago.balves@hotmail.com>
2025-06-08 09:12:36 -0300

    criaçao de constantes referentes aos schemas usuario e pessoa

---
+++ src/main/java/com/pontual_telemetria/pontual_monitor_api/infrastructure/util/Constants.java
@@ -0,0 +1,16 @@
+package com.pontual_telemetria.pontual_monitor_api.infrastructure.util;
+
+public class Constants {
+
+    private Constants() {
+        // private constructor
+    }
+
+    //SCHEMAS
+    public static final String SCHEMA_PERSON = "sch_person";
+    public static final String SCHEMA_USER = "sch_user";
+
+    //TABLES
+    public static final String TABLE_PERSON = "person";
+    public static final String TABLE_ACCOUNT_USER = "account_user";
+}
