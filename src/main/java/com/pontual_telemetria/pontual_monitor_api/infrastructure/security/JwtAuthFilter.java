43c4871c4198dac82d580b222102113c1b3b44b3
Thiago Alves <thiago.balves@hotmail.com>
2025-06-08 14:02:50 -0300

    inclusao de anotacao para remoçao de warning

--- src/main/java/com/pontual_telemetria/pontual_monitor_api/infrastructure/security/JwtAuthFilter.java
+++ src/main/java/com/pontual_telemetria/pontual_monitor_api/infrastructure/security/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -24,7 +25,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final CustomUserDetailService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
 
         if(authHeader != null && authHeader.startsWith("Bearer ")) {
