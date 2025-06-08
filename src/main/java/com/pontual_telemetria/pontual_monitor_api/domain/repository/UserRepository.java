b5ac2614f7f7d7622d59d735bd13e7ffe9f264db
Thiago Alves <thiago.balves@hotmail.com>
2025-06-08 13:58:25 -0300

    criacao de método para recuperar dados de usuario

--- src/main/java/com/pontual_telemetria/pontual_monitor_api/domain/repository/UserRepository.java
+++ src/main/java/com/pontual_telemetria/pontual_monitor_api/domain/repository/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<AccountUser, Long> {
+    AccountUser findByUsername(String username);
     boolean existsByUsername(String username);
 }
