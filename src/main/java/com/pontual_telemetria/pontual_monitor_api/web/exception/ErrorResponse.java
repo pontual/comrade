e275d861c259315b52d81b6501b03345c78f0cab
Thiago Alves <thiago.balves@hotmail.com>
2025-06-13 16:52:28 -0300

    habilitando feign

--- src/main/java/com/pontual_telemetria/pontual_monitor_api/web/exception/ErrorResponse.java
+++ src/main/java/com/pontual_telemetria/pontual_monitor_api/web/exception/ErrorResponse.java
@@ -2,7 +2,6 @@ package com.pontual_telemetria.pontual_monitor_api.web.exception;
 
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @Builder
@@ -10,7 +9,7 @@ public class ErrorResponse {
 
     private String error;
     private String code;
-    private HttpStatus status;
+    private int status;
     private String message;
 
 }
